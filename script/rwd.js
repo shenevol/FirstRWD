7801426cec0de496d5ebb99ac66f0431fa48eda2
yuting <cat3067@gmail.com>
2015-08-22 21:58:43 +0800

    add the transition animation for dropdownlist

--- script/rwd.js
+++ script/rwd.js
@@ -3,4 +3,8 @@ $(document).ready(function(){
 		$("#sidebar li").removeClass("active");
 		$(this).parent().addClass("active");
 	});
+
+	$("#dropdownlist span").click(function(){
+		$("#dropdownlist").toggleClass("active");
+	});
 });
